180eb4cfdc7aec4912ee3d4b9ba653e78985d238
katrien <katrien.colson@gmail.com>
2021-03-23 10:21:04 +0100

    gulp en shellscript

---
+++ shellscript-opgave/init.sh
@@ -0,0 +1,22 @@
+#! usr/bin/bash
+
+# in de gitBash terminal: touch init.sh (nieuwe sh file aanmaken)
+# open daarna vs door code .
+# zet in terminal van vs: chmod +x init.sh
+# daarna: ./init.sh
+
+# OPGAVE:
+#! usr/bin/bash => plaats dit bovenaan in de init.sh
+# dit in vs terminal
+# touch index.html
+# touch about.html
+# touch contact.html
+# mkdir js
+# cd js
+# touch script.js
+# cd ../
+# mkdir css
+# cd css
+# touch style.css
+# cd ../
+# mkdir images
